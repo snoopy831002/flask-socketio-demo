c3d87750fa8a2d4dd19d166556828660c6a54b07
snoopy831002 <snoopy831002@gmail.com>
2021-01-17 11:00:33 +0800

    Add bootstrap notification

--- application.py
+++ application.py
@@ -7,7 +7,6 @@ Inspired by : https://github.com/shanealynn/async_flask
 # Start with a basic flask app webpage.
 from flask_socketio import SocketIO
 from flask import Flask, render_template
-from random import random
 from threading import Thread, Event
 
 app = Flask(__name__)
@@ -17,45 +16,41 @@ app.config['DEBUG'] = True
 #Initiate socketio server
 socketio = SocketIO(app, async_mode=None, logger=True, engineio_logger=True)
 
-#random number Generator Thread
+#socket notification Thread
 thread = Thread()
 thread_stop_event = Event()
 
-def randomNumberGenerator():
+def serverNotification():
     """
-    Generate a random number every 1 second and emit to a socketio instance (broadcast)
-    Ideally to be run in a separate thread?
+    Prototype of server sent notification to web client
     """
-    #infinite loop of magical random numbers
-    print("Making random numbers")
     while not thread_stop_event.isSet():
-        number = round(random()*10, 3)
-        print(number)
         # socket io server send data to client
-        socketio.emit('newnumber', {'number': number}, namespace='/test')
+        socketio.emit('notify', {'number': 0}, namespace='/channel')
         socketio.sleep(1)
 
 
 @app.route('/')
 def index():
-    #only by sending this page first will the client be connected to the socketio instance
+    # only by sending this page first will the client be connected to the socketio instance
     return render_template('index.html')
 
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect', namespace='/channel')
 def test_connect():
     # need visibility of the global thread object
     global thread
     print('Client connected')
 
     #Start the random number generator thread only if the thread has not been started before.
-    if not thread.isAlive():
+    if not thread.is_alive():
         print("Starting Thread")
-        thread = socketio.start_background_task(randomNumberGenerator)
+        thread = socketio.start_background_task(serverNotification)
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect', namespace='/channel')
 def test_disconnect():
     print('Client disconnected')
 
 
 if __name__ == '__main__':
-    socketio.run(app)
+    print("Server running")
+    socketio.run(app,port=5001)
