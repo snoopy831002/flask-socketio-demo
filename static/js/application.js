284ca3f5e762c7aa8c46b2a22915c1aee473048e
snoopy831002 <snoopy831002@gmail.com>
2021-01-24 16:26:30 +0800

    Finish real-time chart

--- static/js/application.js
+++ static/js/application.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
+
+    var time = ((new Date()).getTime() / 1000)|0;
+    var areaChartData = [
+      // The first layer
+      {
+        label: "Layer 1",
+        values: [ {time: time, y: 0},{time: time+1, y: 0} ] // default values
+      },
+
+      // The second layer
+      {
+        label: "Layer 2",
+        values: [ {time: time, y: 0},{time: time+1, y: 0}] // default values
+      },
+    ];
+
+    // create chart
+    var areaChartInstance =  $('#areaChart').epoch({
+        type: 'time.area',
+        data: areaChartData
+    });
+
     //connect to the socket server.
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
     var numbers_received = [];
@@ -6,15 +28,11 @@ $(document).ready(function(){
     //receive details from server
     socket.on('newnumber', function(msg) {
         console.log("Received number" + msg.number);
-        //maintain a list of ten numbers
-        if (numbers_received.length >= 10){
-            numbers_received.shift()
-        }            
-        numbers_received.push(msg.number);
-        numbers_string = '';
-        for (var i = 0; i < numbers_received.length; i++){
-            numbers_string = numbers_string + '<p>' + numbers_received[i].toString() + '</p>';
-        }
-        $('#log').html(numbers_string);
+        var time2 =((new Date()).getTime() / 1000)|0;
+        var updatedData = [
+            {time: time2, y: msg.number*10+20},
+            {time: time2, y:msg.number*10}
+        ];
+        areaChartInstance.push(updatedData);
     });
 });
